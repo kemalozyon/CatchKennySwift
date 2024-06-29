43f0c8cb6ba8172f0cf5ba402266e3f387e4b5e8
Kemal Özyön <kemalozyon51@gmail.com>
2024-06-29 06:26:57 +0300

    Initial Commit

---
+++ CatchKenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchKenny
+//
+//  Created by Kemal Özyön on 29.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
